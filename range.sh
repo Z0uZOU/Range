37a2e9d774ca6c7c00ee319f8743c11200aaa361
FRONTEAU Adrien <zouzou.is.reborn@hotmail.fr>
2020-11-08 17:55:55 +0100

    Fix

--- range.sh
+++ range.sh
@@ -6,7 +6,7 @@
 ## Installation bin: wget -q https://raw.githubusercontent.com/Z0uZOU/Range/master/range.sh -O range.sh && sed -i -e 's/\r//g' range.sh && shc -f range.sh -o range.bin && chmod +x range.bin && rm -f *.x.c && rm -f range.sh
 ## Installation sh: wget -q https://raw.githubusercontent.com/Z0uZOU/Range/master/range.sh -O range.sh && sed -i -e 's/\r//g' range.sh && chmod +x range.sh
 ## Micro-config
-version="Version: 2.0.0.28" #base du système de mise à jour
+version="Version: 2.0.0.29" #base du système de mise à jour
 description="Range et renomme les téléchargements" #description pour le menu
 description_eng="" #description pour le menu
 script_github="https://raw.githubusercontent.com/Z0uZOU/Range/master/range.sh" #emplacement du script original
@@ -1333,7 +1333,7 @@ for dossier in $mes_dossiers_auto ; do
     printf "\rRecherche en cours dans $dossier_source... ${spin:$i:1}"
     sleep .1
   done
-  find "$dossier_source" -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.diz -o -iname \*.txt -o -iname \*.nfo-o -iname \*.db \) -print0 > $dossier_config/tmpfile &
+  find "$dossier_source" -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.diz -o -iname \*.txt -o -iname \*.nfo -o -iname \*.db \) -print0 > $dossier_config/tmpfile &
   pid=$!
   spin='-\|/'
   i=0
